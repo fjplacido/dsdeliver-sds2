d4063494167bbab7214ff17dbe67140426698a28
Francisco Cunha <fj.placido@gmail.com>
2021-01-09 21:18:04 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdelivery/repositories/OrdertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dsdelivery.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.dsdelivery.entities.Order;
-
-public interface OrdertRepository extends JpaRepository<Order, Long> {
-
-}
